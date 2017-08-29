e3fb2cdd2084158ced062c11aeff615064af724a
arnikz <a.kuzniar@esciencecenter.nl>
2017-08-29 14:38:19 +0200

    Bug fixes & enhancements.

--- scripts/split_bam.sh
+++ scripts/split_bam.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -e
 #
 # This script splits a whole-genome BAM file into per-chromosome BAM files
 # on SGE cluster. Note that only proper read pairs are selected per chromosome.
@@ -7,24 +7,34 @@
 BAM_FILE=$1
 THREADS=$2
 
-if [ ! -f $BAM_FILE ]; then
-    echo "$BAM_FILE does not exist."
-    exit 1
+if [ ! -f ${BAM_FILE} ]; then
+  echo "${BAM_FILE} does not exist."
+  exit 1
 fi
 
-if ! [ "$THREADS" -ge 1 ]; then
-   echo "Set the number of threads to a positive integer >= 1."
-   exit 1
+if [ "${THREADS}" -lt 1 ]; then
+  echo "Set the number of threads to a positive integer >= 1."
+  exit 1
 fi
 
-# fetch list of chromosomes from the BAM header
-STR=$(echo $(samtools view -H $BAM_FILE | grep ^@SQ|cut -f 2 | cut -f 2 -d :))
-IFS=' ' read -ra CHR <<< "$STR"
+BAM_PREFIX=$(basename ${BAM_FILE} .bam)
+RTIME=7200 # in sec
+VMEM=100M
 
-for c in "${CHR[@]}"
-do
-    echo "
-    samtools view -bh -@ $THREADS -f 2 -o ${c}.bam VCAP_dedup.realigned.bam $c \
-    && samtools index -@ $THREADS ${c}.bam ${c}.bai
-    " | qsub -N samtools-${THREADS}-chr${c} -cwd -V -l h_rt=02:00:00 -l h_vmem=100M -pe threaded $THREADS
+# fetch a list of chromosomes from the BAM header
+STR=$(echo $(samtools view -H ${BAM_FILE}|grep ^@SQ|cut -f 2|cut -f 2 -d :))
+IFS=' ' read -ra CHR <<< "${STR}"
+
+if [ "${#CHR[@]}" -eq 0 ]; then
+  echo "The list of chromosomes is empty."
+  exit 1
+fi
+
+for c in "${CHR[@]}"; do
+  CHR_BAM_FILE=${BAM_PREFIX}-chr${c}.bam
+  echo "
+    samtools view -bh -@ ${THREADS} -f 2 -o ${CHR_BAM_FILE} ${BAM_FILE} ${c} \
+    && samtools index -@ ${THREADS} ${CHR_BAM_FILE} ${c}.bai"| \
+    qsub -N samtools-${THREADS}-${CHR_BAM_FILE} -cwd -V \
+    -l h_rt=$((${RTIME}/${THREADS})) -l h_vmem=${VMEM} -pe threaded ${THREADS}
 done
