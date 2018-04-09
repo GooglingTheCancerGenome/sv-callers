954af79267280ee3526c2dda242e55b8c50319d9
arnikz <a.kuzniar@esciencecenter.nl>
2018-04-10 01:18:18 +0200

    Update views

--- scripts/stat_jobs.py
+++ scripts/stat_jobs.py
@@ -35,8 +35,8 @@ CREATE VIEW {4} AS
 SELECT
     jobid,
     jobname,
-    start,
-    end,
+    strftime('%Y-%m-%d %H:%M:%D', start) AS start,
+    strftime('%Y-%m-%d %H:%M:%D', end) AS end,
     strftime('%s',end) - strftime('%s', start) AS rtime_s,
     strftime('%Y-%m-%d %H:00:00', start) AS start_bin,
     strftime('%Y-%m-%d %H:00:00', end) AS end_bin,
@@ -56,8 +56,20 @@ SELECT
     MAX(mem_mb) AS max_mem,
     CAST(ROUND(AVG(mem_mb)) AS INT) AS mean_mem
 FROM {4}
-GROUP BY jobname, state
-""".format('TMP', sep, csvfile, 'JOB', 'V_JOB', 'VV_JOB')
+GROUP BY jobname, state;
+
+CREATE VIEW {6} AS
+SELECT end_bin, state, COUNT(*) AS n_jobs
+FROM {4}
+GROUP BY end_bin, state ORDER BY 1;
+
+CREATE VIEW {7} AS
+SELECT T1.end_bin, "end", T1.state, T1.n_jobs, SUM(T2.n_jobs) AS cum_jobs
+FROM {6} AS T1, V_JOB_E AS T2
+WHERE T2.rowid <= T1.rowid AND T2.state = T1.state
+GROUP BY T1.state, T1.end_bin, T1.n_jobs
+ORDER BY T1.state, T1.rowid;
+""".format('TMP', sep, csvfile, 'JOB', 'V_JOB', 'VV_JOB', 'V_JOB_E', 'VV_JOB_E')
 
 with open(sqlfile, "w") as fout:
     fout.write(stmts)
@@ -66,33 +78,3 @@ cmd = "sacct -u akuzniar -r normal -S {} -E {} -P --delimiter={} \
       -o jobid,jobname,start,end,maxvmsize,state > {} && \
       sqlite3 {} < {}".format(period[0], period[1], sep, csvfile, dbfile, sqlfile)
 os.system(cmd)
-
-"""
-CREATE VIEW V_JOB_S AS
-SELECT start_bin, COUNT(*) AS n_jobs
-FROM V_JOB
-GROUP BY start_bin ORDER BY 1;
-
-CREATE VIEW V_JOB_E AS
-SELECT end_bin, COUNT(*) AS n_jobs
-FROM V_JOB
-GROUP BY end_bin, state ORDER BY 1;
-
-CREATE VIEW VV_JOB_S AS
-SELECT T1.start_bin, "start", T1.n_jobs, SUM(T2.n_jobs)
-FROM V_JOB_S AS T1, V_JOB_S AS T2
-WHERE T2.rowid <= T1.rowid
-GROUP BY T1.start_bin, T1.n_jobs
-ORDER BY T1.rowid;
-
-CREATE VIEW VV_JOB_E AS
-SELECT T1.end_bin, "end", T1.n_jobs, SUM(T2.n_jobs)
-FROM V_JOB_E AS T1, V_JOB_E AS T2
-WHERE T2.rowid <= T1.rowid
-GROUP BY T1.end_bin, T1.n_jobs
-ORDER BY T1.rowid;
-
-SELECT * FROM VV_JOB_S
-UNION
-SELECT * FROM VV_JOB_E;
-"""
