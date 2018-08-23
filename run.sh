ab4548ec0587909252906f50be70548d1b1a5927
arnikz <a.kuzniar@esciencecenter.nl>
2018-08-23 02:07:04 +0200

    Add script to run the workflow

---
+++ run.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+source ~/.profile && \
+cd sv-callers/snakemake && \
+git checkout dev && \
+snakemake -C echo_run=1 mode=$1 --latency-wait 30 --jobs \
+  --cluster "xenon -vvv scheduler $2 --location local:// submit \
+  --name smk.{rule} --inherit-env --max-run-time 1 --working-directory ."
