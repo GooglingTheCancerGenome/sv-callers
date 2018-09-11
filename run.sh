6f0c16e85315f179b205d52d4e4edf1d291f74fc
arnikz <a.kuzniar@esciencecenter.nl>
2018-09-11 22:52:13 +0200

    Clone sv-callers repo during CI.

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 source ~/.profile && \
+git lfs clone -b dev https://github.com/GooglingTheCancerGenome/sv-callers.git && \
 cd sv-callers/snakemake && \
-git checkout dev && \
 snakemake -C echo_run=0 enable_callers="['manta']" mode=$1 --latency-wait 30 --jobs \
-  --cluster "xenon -vvvv scheduler $2 --location local:// submit \
+  --cluster "xenon -vvv scheduler $2 --location local:// submit \
   --name smk.{rule} --inherit-env --max-run-time 15 --working-directory ."
