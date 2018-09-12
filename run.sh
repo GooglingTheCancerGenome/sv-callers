0dd7a92033b78df42e8578011c1aa51fa850769c
arnikz <a.kuzniar@esciencecenter.nl>
2018-09-12 11:57:04 +0200

    Debug: sv calling

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ git lfs clone -b dev https://github.com/GooglingTheCancerGenome/sv-callers.git &
 cd sv-callers/snakemake && \
 ls -lh data/bam/3 && \
 ls -lh data/fasta && \
-snakemake -C echo_run=1 enable_callers="['manta','delly','lumpy','gridss']" \
+snakemake -C echo_run=0 enable_callers="['manta','delly','lumpy','gridss']" \
   mode=$1 --latency-wait 30 --jobs \
   --cluster "xenon -vvv scheduler $2 --location local:// submit \
   --name smk.{rule} --inherit-env --max-run-time 15 --working-directory ."
