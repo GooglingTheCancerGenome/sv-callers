e2b2e0f8638ef7a9327021445fff7ce778cc6eb6
arnikz <a.kuzniar@esciencecenter.nl>
2018-09-14 11:21:34 +0200

    Run callers single-threaded.

--- run.sh
+++ run.sh
@@ -1,11 +1,9 @@
-#!/bin/bash
+#!/bin/bash -xe
 
-source ~/.profile && \
-git lfs clone -b dev https://github.com/GooglingTheCancerGenome/sv-callers.git && \
-cd sv-callers/snakemake && \
-ls -lh data/bam/3 && \
-ls -lh data/fasta && \
+source ~/.profile
+git lfs clone -b dev https://github.com/GooglingTheCancerGenome/sv-callers.git
+cd sv-callers/snakemake
 snakemake -C echo_run=0 enable_callers="['manta','delly','lumpy','gridss']" \
   mode=$1 --use-conda --latency-wait 30 --jobs \
   --cluster "xenon -vvv scheduler $2 --location local:// submit \
-  --name smk.{rule} --inherit-env --max-run-time 15 --working-directory . --stderr stderr-%j.log --stdout stdout-%j.log"
+  --name smk.{rule} --procs-per-node=1 --start-single-process --inherit-env --max-run-time 15 --working-directory ."
