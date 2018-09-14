987f42f4ea41a39e8fec000aa78fdedc6daceaf1
arnikz <a.kuzniar@esciencecenter.nl>
2018-09-14 11:05:11 +0200

    Use conda env

--- run.sh
+++ run.sh
@@ -6,6 +6,6 @@ cd sv-callers/snakemake && \
 ls -lh data/bam/3 && \
 ls -lh data/fasta && \
 snakemake -C echo_run=0 enable_callers="['manta','delly','lumpy','gridss']" \
-  mode=$1 --latency-wait 30 --jobs \
+  mode=$1 --use-conda --latency-wait 30 --jobs \
   --cluster "xenon -vvv scheduler $2 --location local:// submit \
-  --name smk.{rule} --inherit-env --max-run-time 15 --working-directory ."
+  --name smk.{rule} --inherit-env --max-run-time 15 --working-directory . --stderr stderr-%j.log --stdout stdout-%j.log"
