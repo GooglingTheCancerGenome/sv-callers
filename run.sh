07c075ace8185ef30933aae2fec0793fdee91d9d
arnikz <a.kuzniar@esciencecenter.nl>
2018-09-24 20:38:18 +0200

    Count SVs in *.vcf files.

--- run.sh
+++ run.sh
@@ -7,3 +7,5 @@ snakemake -C echo_run=0 enable_callers="['manta','delly','lumpy','gridss']" \
   mode=$1 --use-conda --latency-wait 30 --jobs \
   --cluster "xenon -vvv scheduler $2 --location local:// submit \
   --name smk.{rule} --procs-per-node=1 --start-single-process --inherit-env --max-run-time 15 --working-directory ."
+find data -name workspace | xargs rm -fr
+find data -name *.vcf | xargs grep -cv "#"
