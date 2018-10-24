9f912de9d33548794018923ec10edd999a27187a
Arnold Kuzniar <a.kuzniar@esciencecenter.nl>
2018-10-24 16:36:11 +0200

    Fix travis: exit with non-zero if no VCF output per caller.

--- run.sh
+++ run.sh
@@ -4,21 +4,25 @@ source ~/.profile
 git clone -b dev https://github.com/GooglingTheCancerGenome/sv-callers.git
 cd sv-callers/snakemake
 
-CALLERS="['manta','delly','lumpy','gridss']"
+CALLERS=(manta delly lumpy gridss)
+STR_CALLERS="[$(printf "'%s'," "${CALLERS[@]}"|sed 's/,$//')]"
 ECHO=$1
 MODE=$2
 SCH=$3
 SAMPLES=$([ "$ECHO" -eq "1" ] && echo "samples_dummy.csv" || echo "samples.csv")
 USE_CONDA=$([ "$ECHO" -eq "0" ] && echo "--use-conda" || echo "")
 
+echo "Selected callers: $STR_CALLERS"
 snakemake -C echo_run=$ECHO samples=$SAMPLES mode=$MODE \
-  enable_callers="$CALLERS" $USE_CONDA \
+  enable_callers="$STR_CALLERS" $USE_CONDA \
   --latency-wait 30 --jobs \
   --cluster "xenon -vvv scheduler $SCH --location local:// submit \
   --name smk.{rule} --procs-per-node=1 --start-single-process --inherit-env \
   --max-run-time 15 --working-directory ."
 
 if [ "$ECHO" -eq "0" ]; then
-  find data -name workspace | xargs rm -fr
-  find data -name *.vcf | xargs grep -cv "#"
+  for c in "${CALLERS[@]}"; do
+    vcf_file="$c.vcf"
+    find data -name $vcf_file | xargs grep -cv "#" || exit 1
+  done
 fi
