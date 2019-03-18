52073d953aa42bfc39631a13c5fd23357beaf105
Arnold Kuzniar <a.kuzniar@esciencecenter.nl>
2019-03-18 14:32:01 +0100

    Add cputime & improve formatting.

--- scripts/stat_jobdb.py
+++ scripts/stat_jobdb.py
@@ -9,29 +9,13 @@ import datetime
 
 # test user input
 if len(sys.argv) == 1:
-   print("Usage: %s [dbfile]" % sys.argv[0])
-   sys.exit(2)
+    print("Usage: %s [dbfile]" % sys.argv[0])
+    sys.exit(2)
 
 dbfile = sys.argv[1]
 
-# user-defined (aggregate) functions (UDFs); https://docs.python.org/2/library/sqlite3.html
-def log(value, base):
-    try:
-        return math.log(value) / math.log(base)
-    except:
-        return None
-
-class Stdev: # sample standard deviation (aggregate function)
-    def __init__(self):
-        self.arr = []
-
-    def step(self, value):
-        self.arr.append(value)
-
-    def finalize(self):
-        return np.std(np.array(self.arr), ddof=1)
-
-class Median: # median (aggregate function)
+# user-defined function (UDF)
+class Median:
     def __init__(self):
         self.arr = []
 
@@ -41,45 +25,33 @@ class Median: # median (aggregate function)
     def finalize(self):
         return np.median(np.array(self.arr))
 
-class Mad: # median absolute deviation (aggregate function)
-    def __init__(self):
-        self.arr = []
-
-    def step(self, value):
-        self.arr.append(value)
-
-    def finalize(self):
-        median = np.median(np.array(self.arr))
-        return np.median(np.abs(self.arr - median))
-
 # connect to db
 with sqlt.connect(dbfile) as conn:
-   conn.row_factory = sqlt.Row # enable column access by name: row['colnm']
-   conn.create_function('log', 2, log)
-   conn.create_aggregate('stdev', 1, Stdev)
-   conn.create_aggregate('median', 1, Median)
-   conn.create_aggregate('mad', 1, Mad)
-   cur = conn.cursor()
-   log_base = 2
-   precision = 4
-   sql_stmt = """
-   SELECT
+    conn.row_factory = sqlt.Row  # enable column access by name: row['colnm']
+    conn.create_aggregate('median', 1, Median)
+    cur = conn.cursor()
+    sql_stmt = """
+    SELECT
       jobname,
       COUNT(jobid) AS n,
-      ROUND(MIN(runtime) / 60, 0) AS min,
-      ROUND(MAX(runtime) / 60, 0) AS max,
-      ROUND(AVG(runtime) / 60, 0) AS mean,
-      ROUND(MEDIAN(runtime) / 60, 0) AS median
-   FROM V_JOB
-   WHERE status = 'COMPLETED'
-   GROUP BY jobname;
-   """
+      ROUND(MIN(runtime), 0) AS min_rt, -- in sec
+      ROUND(MAX(runtime), 0) AS max_rt,
+      ROUND(AVG(runtime), 0) AS mean_rt,
+      ROUND(MEDIAN(runtime), 0) AS median_rt,
+      ROUND(MIN(cputime), 0) AS min_ct, -- in sec
+      ROUND(MAX(cputime), 0) AS max_ct,
+      ROUND(AVG(cputime), 0) AS mean_ct,
+      ROUND(MEDIAN(cputime), 0) AS median_ct
+    FROM V_JOB
+    WHERE status = 'COMPLETED'
+    GROUP BY jobname"""
 
-   cur.execute(sql_stmt)
-   for row in cur.fetchall():
-       for k in row.keys():
-          v = str(row[k])
-#          if k not in ['jobname','n']:
-#              v += " ({})".format(str(datetime.timedelta(seconds=int(row[k]))))
-          print("%s:\t%s" % (k, v))
-       print("\n")
+    cur.execute(sql_stmt)
+    for row in cur.fetchall():
+        for k in row.keys():
+            v = row[k]
+            if k in ('jobname'):
+                print("%s: %s" % (k, v))
+            else:
+                print("%s: %d [%s]" % (k, v, datetime.timedelta(seconds=v)))
+        print("\n")
