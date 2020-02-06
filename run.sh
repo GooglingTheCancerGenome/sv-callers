5cf5dddc5250fd5529168078a8440908acc08596
Arnold Kuzniar <a.kuzniar@esciencecenter.nl>
2020-02-06 20:36:29 +0100

    Query/parse job accounting info using xenon/jq.

--- run.sh
+++ run.sh
@@ -2,19 +2,34 @@
 
 set -xe
 
-CALLERS=(manta delly lumpy gridss)
-STR_CALLERS="[$(printf "'%s'," "${CALLERS[@]}"|sed 's/,$//')]"
-EXIT_CODE=0
+# check input arg(s)
+if [ $# -lt "3" ]; then
+  echo "Usage: $0 [ECHO run] [MODE {s,p}] [SCHEDULER {gridengine,slurm}]"
+  exit 1
+fi
+
 ECHO=$1
 MODE=$2
 SCH=$3
-SAMPLES="samples.csv"
+EXIT_CODE=0
+CALLERS=(manta delly lumpy gridss)
+STR_CALLERS="[$(printf "'%s'," "${CALLERS[@]}"|sed 's/,$//')]"
+JOBS=() # array of job IDs
+JOBS_LOG=jobs.json # job accounting log
+SAMPLES=samples.csv
 USE_CONDA=$([ "$ECHO" -eq "0" ] && echo "--use-conda" || echo "")
 MY_ENV=wf
 
+monitor () {  # monitor a job via Xenon CLI
+  xenon -v --json scheduler $SCH --location local:// list --identifier $1
+}
+
+# activate conda env
 eval "$(conda shell.bash hook)"
 conda activate $MY_ENV
 conda list
+
+# run workflow
 cd snakemake && ls -alh
 echo "Selected callers: $STR_CALLERS"
 snakemake -C echo_run=$ECHO samples=$SAMPLES mode=$MODE \
@@ -26,25 +41,35 @@ snakemake -C echo_run=$ECHO samples=$SAMPLES mode=$MODE \
   --max-run-time 15 --working-directory . \
   --stderr stderr-%j.log --stdout stdout-%j.log"
 
-echo -e "\nVCF output files:"
+# check SV callers' output in VCF files
+echo "-----------------"
+echo "VCF output files:"
 if [ "$ECHO" -eq "0" ]; then
   for caller in "${CALLERS[@]}"; do
     VCF_FILE="$(find data -mindepth 6 -name $caller.vcf)"
     BOOL=$([ -e  "$VCF_FILE" ] && echo 0 || echo 1)
     INFO=$([ $BOOL -eq 0 ] && echo "$VCF_FILE" || echo "None")
-    if [ $BOOL -gt $EXIT_CODE ]; then
-      EXIT_CODE=1
-    fi
     echo " $caller: $INFO"
   done
   echo " merge:" $(find data -name all.vcf)
 fi
 
-echo -e "\nLog files:"
-ls *.log
+# show logs
+echo "----------"
+echo "Log files:"
 for f in *.log; do
   echo -e "\n### $f ###\n"
   cat $f
+  JOB_ID=$(echo $f | grep err | sed 's/\w*-\([0-9]*\).\w*/\1/')
+  JOBS+=($JOB_ID)
 done
 
-exit $EXIT_CODE
+# collect job accounting info
+for j in ${JOBS[@]}; do
+  monitor $j >> $JOBS_LOG
+done
+cat $JOBS_LOG
+
+# exit with non-zero if there are failed jobs
+[[ $(jq ".statuses | .[] | select(.done==true and .exitCode!=0)" $JOBS_LOG) ]] \
+  && exit 1 || exit 0
