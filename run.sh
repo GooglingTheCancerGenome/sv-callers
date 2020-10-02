7f7439a267e2b0d553a3b1c87accdf788644bc74
arnikz <a.kuzniar@esciencecenter.nl>
2020-10-02 19:01:09 +0200

    Fix wrapper: edit config.

--- run.sh
+++ run.sh
@@ -8,9 +8,9 @@ if [ $# -lt "3" ]; then
   exit 1
 fi
 
-ECHO=$1
-MODE=$2
-SCH=$3
+ECHO="$1"
+MODE="$2"
+SCH="$3"
 CALLERS=(manta delly lumpy gridss)
 STR_CALLERS="[$(printf "'%s'," "${CALLERS[@]}"|sed 's/,$//')]"
 THREADS=1
@@ -21,33 +21,36 @@ USE_CONDA=$([ "$ECHO" -eq "0" ] && echo "--use-conda" || echo "")
 MY_ENV=wf
 
 monitor () {  # monitor a job via Xenon CLI
-  xenon --json scheduler $SCH --location local:// list --identifier $1
+  xenon --json scheduler "$SCH" --location local:// list --identifier "$1"
 }
 
 edit() {  # edit config file
   if [ "$ECHO" == "1" ]; then
-    sed -i.org 's/echo_run:\s*0/echo_run: 1/' $CONFIG
+    sed -i.org 's/echo_run:\s*0/echo_run: 1/' "$1"
   else
-    sed -i.org 's/echo_run:\s*1/echo_run: 0/' $CONFIG
+    sed -i.org 's/echo_run:\s*1/echo_run: 0/' "$1"
   fi
 
-  if [ "$MODE" == 'p' ]; then
-    sed -i.org 's/mode:\s*s/mode: p/' $CONFIG
+  if [ "$MODE" == "p" ]; then
+    sed -i.org 's/mode:\s*s/mode: p/' "$CONFIG"
   else
-    sed -i.org 's/mode:\s*p/mode: s/' $CONFIG
+    sed -i.org 's/mode:\s*p/mode: s/' "$CONFIG"
   fi
 }
 
 # activate conda env
 eval "$(conda shell.bash hook)"
-conda activate $MY_ENV
+conda activate "$MY_ENV"
 conda list
 
+# edit CONFIG
+cd ./snakemake && ls -alh
+edit "$CONFIG"
+
 # run workflow
-cd snakemake && ls -alh
 echo "Selected callers: $STR_CALLERS"
-snakemake $USE_CONDA \
-  --configfile $CONFIG \
+snakemake "$USE_CONDA" \
+  --configfile "$CONFIG" \
   --latency-wait 60 --jobs \
   --cluster "xenon -vvv scheduler $SCH --location local:// submit \
   --name smk.{rule} --cores-per-task $THREADS --inherit-env \
@@ -61,7 +64,7 @@ if [ "$ECHO" -eq "0" ]; then
   for caller in "${CALLERS[@]}"; do
     VCF_FILE="$(find data -mindepth 6 -name $caller.vcf)"
     BOOL=$([ -e  "$VCF_FILE" ] && echo 0 || echo 1)
-    INFO=$([ $BOOL -eq 0 ] && echo "$VCF_FILE" || echo "None")
+    INFO=$([ "$BOOL" -eq "0" ] && echo "$VCF_FILE" || echo "None")
     echo " $caller: $INFO"
   done
   echo " merge:" $(find data -name all.vcf)
@@ -72,17 +75,17 @@ echo "----------"
 echo "Log files:"
 for f in *.log; do
   echo -e "\n### $f ###\n"
-  cat $f
-  JOB_ID=$(echo $f | grep err | sed 's/\w*-\([0-9]*\).\w*/\1/')
+  cat "$f"
+  JOB_ID=$(echo "$f" | grep err | sed 's/\w*-\([0-9]*\).\w*/\1/')
   JOBS+=($JOB_ID)
 done
 
 # collect job accounting info
 for j in ${JOBS[@]}; do
-  monitor $j > jobacct-$j.json
+  monitor "$j" > "jobacct-$j.json"
 done
-cat *.json
+cat ./*.json
 
 # exit with non-zero if there are failed jobs
-[[ $(jq ".statuses | .[] | select(.done==true and .exitCode!=0)" *.json) ]] \
+[[ $(jq ".statuses | .[] | select(.done==true and .exitCode!=0)" ./*.json) ]] \
   && exit 1 || exit 0
