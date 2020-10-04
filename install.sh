c2c2b87eba496424a75dcb35cfcf8369dbdbd216
arnikz <a.kuzniar@esciencecenter.nl>
2020-10-04 16:43:48 +0200

    Fix CI: add wf deps #46.

--- install.sh
+++ install.sh
@@ -9,3 +9,6 @@ printenv
 conda env create -n $MY_ENV -f environment.yaml
 conda activate $MY_ENV
 conda list
+cd ./snakemake
+echo " - bc=1.06" >> environment.yaml
+snakemake --use-conda --create-envs-only --cores 1
