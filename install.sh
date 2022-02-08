1f7a0b5910cb0bc047f9c5e2267fd40654bf0814
arnikz <arnold.kuzniar@gmail.com>
2022-02-08 23:57:21 +0100

    Install deps via conda/mamba.

--- install.sh
+++ install.sh
@@ -6,7 +6,9 @@ MY_ENV=wf
 
 eval "$(conda shell.bash hook)"
 printenv
-conda env create -n $MY_ENV -f environment.yaml
+conda install -n base -c conda-forge mamba
+conda activate base
+mamba env create -n $MY_ENV -f environment.yaml
 conda activate $MY_ENV
 conda list
 cd ./workflow
