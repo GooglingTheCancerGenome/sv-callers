e777f480194ad48b2dece122ea8db6201f76f94d
= <arnold.kuzniar@gmail.com>
2022-05-16 00:54:29 +0200

    List pkgs in base.

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ eval "$(conda shell.bash hook)"
 printenv
 conda install -n base -c conda-forge mamba
 conda activate base
+conda list
 mamba env create -n $MY_ENV -f environment.yaml
 conda activate $MY_ENV
 conda list
